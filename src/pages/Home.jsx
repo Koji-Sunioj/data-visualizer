13447a68b72a0bc8003982667adc84efe0ea6ba8
Koji-Sunioj <koji.gabriel218@gmail.com>
2024-04-21 15:17:22 +0300

    something

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Row, Col } from "solid-bootstrap";
+
+const Home = () => {
+  return (
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <h1>Welcome bitch</h1>
+      </Col>
+    </Row>
+  );
+};
+
+export default Home;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,55 @@
+import { Form, Button, Row, Col } from "solid-bootstrap";
+
+const SignIn = () => {
+  const logIn = async (event) => {
+    event.preventDefault();
+
+    const {
+      target: {
+        email: { value: email },
+        password: { value: password },
+      },
+    } = event;
+
+    const url = "http://localhost:8000/sign-in";
+
+    const request = await fetch(url, {
+      body: JSON.stringify({ email: email, password: password }),
+      method: "POST",
+    });
+
+    const response = await request.json();
+    console.log(response.token);
+  };
+
+  return (
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Form
+          style={{ marginTop: "20px" }}
+          data-bs-theme="light"
+          onSubmit={logIn}
+        >
+          <h2>Sign in</h2>
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignIn;
